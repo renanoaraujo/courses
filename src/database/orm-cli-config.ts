c3a8bc94086c84ec750fbcae488301a9c9541ac4
Renan Araujo <renan.o.araujo@gmail.com>
2024-01-19 23:40:06 -0300

    database created by migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,18 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1705695451069 } from 'src/migrations/1705695451069-CreateCoursesTable';
 import { CreateTagsTable1705697190833 } from 'src/migrations/1705697190833-CreateTagsTable';
+import { CreateCoursesTagsTable1705713655387 } from 'src/migrations/1705713655387-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1705714377230 } from 'src/migrations/1705714377230-AddCoursesIdToCoursesTagsTable';
+import { AddTagsIdToCoursesTagsTable1705715713816 } from 'src/migrations/1705715713816-AddTagsIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1705695451069, CreateTagsTable1705697190833],
+  migrations: [
+    CreateCoursesTable1705695451069,
+    CreateTagsTable1705697190833,
+    CreateCoursesTagsTable1705713655387,
+    AddCoursesIdToCoursesTagsTable1705714377230,
+    AddTagsIdToCoursesTagsTable1705715713816,
+  ],
 });
